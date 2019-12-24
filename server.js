e867ee03ba25a26b6b9f4d5cb14a487f8fe37266
nadjarben <nadjarbenj@gmail.com>
2019-12-24 19:01:49 +0200

    test2 sw prod

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const formRoutes = require('./routes/form');
 // app
 const app = express();
 
+//sw
+app.get("/service-worker.js", (req, res) => {
+    // Don't cache service worker is a best practice (otherwise clients wont get emergency bug fix)
+    res.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    res.set("Content-Type", "application/javascript");
+    app.serveStatic(req, res, path.resolve("./.next/service-worker.js"));
+});
+
 // db
 mongoose
     .connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
@@ -46,10 +54,3 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-//sw
-app.get("/service-worker.js", (req, res) => {
-    // Don't cache service worker is a best practice (otherwise clients wont get emergency bug fix)
-    res.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-    res.set("Content-Type", "application/javascript");
-    app.serveStatic(req, res, path.resolve("../frontend/.next/sw.js"));
-});
