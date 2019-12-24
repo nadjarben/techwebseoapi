68ca6945ad77ce7bf43906f6c5d2425afb88875b
nadjarben <nadjarbenj@gmail.com>
2019-12-24 22:47:29 +0200

    reverse last

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const next = require('next');
-const { createServer } = require('http')
 require('dotenv').config();
 // bring routes
 const blogRoutes = require('./routes/blog');
@@ -16,7 +14,7 @@ const tagRoutes = require('./routes/tag');
 const formRoutes = require('./routes/form');
 
 // app
-const server = express();
+const app = express();
 
 // db
 mongoose
@@ -27,45 +25,23 @@ mongoose
     });
 
 // middlewares
-server.use(morgan('dev'));
-server.use(bodyParser.json());
-server.use(cookieParser());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 // cors
 if (process.env.NODE_ENV === 'development') {
-    server.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 // routes middleware
-server.use('/api', blogRoutes);
-server.use('/api', authRoutes);
-server.use('/api', userRoutes);
-server.use('/api', categoryRoutes);
-server.use('/api', tagRoutes);
-server.use('/api', formRoutes);
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', tagRoutes);
+app.use('/api', formRoutes);
 
 // port
-//const port = process.env.PORT || 8000;
-//app.listen(port, () => {
-//    console.log(`Server is running on port ${port}`);
-//});
-const port = parseInt(process.env.PORT, 10) || 3000;
-const app = next({
-    dev: process.env.NODE_ENV !== 'production',
-    dir: '../frontend',
-  });
-  const handle = app.getRequestHandler()
-
-  app.prepare()
-    .then(() => {
-      createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
-        const { pathname } = parsedUrl
-  
-        // handle GET request to /service-worker.js
-          const filePath = ('../frontend/.next/service-worker.js')
-  
-          app.serveStatic(req, res, filePath)
-          })
-      .listen(8000, () => {
-        console.log(`> Ready on http://localhost:${8000}`)
-      })
-    })
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
